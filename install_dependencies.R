63937b8f52a15e04b88ec5ce3c9d27387b0ded6f
MaximilianPi <maximilian.pichler@biologie.uni-regensburg.de>
2021-07-24 16:41:45 +0200

    Remove MacOS and clean project

--- install_dependencies.R
+++ install_dependencies.R
@@ -1,5 +1,10 @@
 source("book/dependencies.R")
 install.packages("keras")
-keras::install_keras()
+# It's annoying to setup keras 
+try({ 
+  reticulate::install_miniconda()
+  keras::install_keras() 
+  reticulate::use_condaenv("r-reticulate")
+  }, silent = TRUE)
 install.packages("torch")
 torch::install_torch()
