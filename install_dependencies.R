fc27d7058c67522bb56a68ba13345a4dc5edcaad
MaximilianPi <maximilian.pichler@biologie.uni-regensburg.de>
2021-11-19 10:27:59 +0100

    License+small final changes

--- install_dependencies.R
+++ install_dependencies.R
@@ -5,6 +5,7 @@ try({
   reticulate::install_miniconda()
   keras::install_keras() 
   reticulate::use_condaenv("r-reticulate")
+  reticulate::conda_install("r-reticulate", packages = "tensorflow_probability", pip = TRUE)
   }, silent = TRUE)
 install.packages("torch")
 torch::install_torch()
